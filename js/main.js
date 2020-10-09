3ce55a3971daf55004aa260df04a6b0ad527ffbd
AramBadalyan <Arampitek@gmail.com>
2020-10-09 11:08:20 +0300

    Finished task1

--- js/main.js
+++ js/main.js
@@ -31,7 +31,20 @@ const OFFER_PHOTOS = [
   `http://o0.github.io/assets/images/tokyo/hotel3.jpg`
 ];
 const AVATAR_PATH = `img/avatars/`;
+const PIN_WIDTH = 50;
+const PIN_HEIGHT = 70;
+const OFFSET_X = PIN_WIDTH / 2;
+const OFFSET_Y = PIN_HEIGHT;
+const MAP_WIDTH = document.querySelector(`.map`).clientWidth;
+const LOC_Y_MIN = 130;
+const LOC_Y_MAX = 630;
 
+const MAP_PINS = document.querySelector(`.map__pins`);
+const PIN_TEMPLATE = document.querySelector(`#pin`)
+  .content
+  .querySelector(`.map__pin`);
+
+// Функции
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
 };
@@ -40,7 +53,10 @@ const getRandomDescription = (possibleOptions) => {
   let mutableOptions = possibleOptions.slice(); // Создаем копию входящего массива
   let result = [];
 
+  // Пробегаем рандомную длину от массива опций
   for (let i = 0, l = getRandomInt(possibleOptions.length); i < l; i++) {
+    // Удаляем опции из копии массива, чтобы не повторяться
+    // В результат записываем нулевой(единственный) элемент вырезанного массива
     result[i] = mutableOptions.splice(getRandomInt(mutableOptions.length), 1)[0];
   }
 
@@ -51,22 +67,29 @@ const getSimilarOffers = (offersNumber) => {
   const similarOffers = [];
   let avatarFiles = [];
 
+  // Генерируем названия файлов аватаров
   for (let i = 0; i < offersNumber; i++) {
     avatarFiles[i] = `user0${i + 1}.png`;
   }
 
   for (let i = 0; i < offersNumber; i++) {
+    // Определяем координаты кончика указателя вычитанием смещения
+    let locationX = getRandomInt(MAP_WIDTH) - OFFSET_X;
+    // Ограничение по Y от LOC_Y_MIN до LOC_Y_MAX
+    let locationY = getRandomInt(LOC_Y_MAX - LOC_Y_MIN) + LOC_Y_MIN - OFFSET_Y;
+
     similarOffers[i] = {
       author: {
-        avatar: `${AVATAR_PATH}${avatarFiles.splice(getRandomInt(avatarFiles.length), 1)}`
+        // При присвоении значения удаляем файл из массива, чтобы не использовать повторно
+        avatar: `${AVATAR_PATH}${avatarFiles.splice(getRandomInt(avatarFiles.length), 1)[0]}`
       },
       location: {
-        x: `размеры блока`,
-        y: getRandomInt(500) + 130
+        x: locationX,
+        y: locationY
       },
       offer: {
         title: `Заголовок предложения`,
-        addres: `${location.x}, ${location.y}`,
+        address: `${locationX}, ${locationY}`,
         price: getRandomInt(20) * 1000,
         type: OFFER_TYPE[getRandomInt(OFFER_TYPE.length)],
         rooms: getRandomInt(10),
@@ -83,5 +106,28 @@ const getSimilarOffers = (offersNumber) => {
   return similarOffers;
 };
 
-let offers = getSimilarOffers(OFFERS_NUMBER);
-console.log(offers);
+const renderPin = (pin) => {
+  const pinElement = PIN_TEMPLATE.cloneNode(true);
+  const pinElementImage = pinElement.querySelector(`img`);
+
+  pinElement.style = `left: ${pin.location.x}px; top: ${pin.location.y}px`;
+  pinElementImage.src = pin.author.avatar;
+  pinElementImage.alt = pin.offer.title;
+
+  return pinElement;
+};
+
+const fillElement = (items) => {
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < items.length; i++) {
+    fragment.appendChild(renderPin(items[i]));
+  }
+
+  return fragment;
+};
+
+const nearbyOffers = getSimilarOffers(OFFERS_NUMBER);
+const mapPins = fillElement(nearbyOffers);
+
+MAP_PINS.appendChild(mapPins);
