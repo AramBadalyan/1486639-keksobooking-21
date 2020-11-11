b221d570431ad3e4436afca68150941495b94736
AramBadalyan <Arampitek@gmail.com>
2020-11-11 23:05:07 +0300

    Added all filters

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 window.page.disactivatePage();
 window.constants.mainPin.addEventListener(`mousedown`, window.pin.movePin);
 
-window.constants.MAP.addEventListener(`click`, function (evt) {
+window.pin.MAP_PINS.addEventListener(`click`, function (evt) {
   evt.preventDefault();
   window.pin.pinClickHandler(evt);
 });
